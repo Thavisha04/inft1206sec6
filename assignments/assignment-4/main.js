5e4e6f05ddc3ddb5c9ce5fd3b5f04911cc68daf2
Thavisha04 <thavisha.thanippuliappuhamilag@dcmail.ca>
2024-07-10 10:14:59 -0400

    a4p1 main.js version 2
    Added the comment header.

--- assignments/assignment-4/main.js
+++ assignments/assignment-4/main.js
@@ -1,3 +1,12 @@
+/*    
+  Name:           Thavisha Thanippuli Appuhamilage
+  Student #:      100942614
+  File:           main.js
+  Date created:   July 10th, 2024
+  Last Modified:  ???
+  Section:        6
+  Description:    This assignment includes the code for creating an app that generates random silly stories.
+*/
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -6,3 +15,5 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
