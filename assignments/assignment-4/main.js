8bc8d19a0e1fe2eea572064e8c11317cc2800c0b
Thavisha04 <thavisha.thanippuliappuhamilag@dcmail.ca>
2024-07-10 15:39:20 -0400

    a1p4 main.js version 7

--- assignments/assignment-4/main.js
+++ assignments/assignment-4/main.js
@@ -43,7 +43,7 @@ function result() {
 
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round(300/14)} stone`;
-    const temperature =  `${((Math.round(94-32))*5)/9} centigrade`;
+    const temperature =  `${Math.round(((94-32)*5)/9)} centigrade`;
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
     newStory = newStory.replaceAll('300 pounds', weight);
 
