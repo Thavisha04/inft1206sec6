ce507841c89f53b4f4045e228d9210d87603c9dc
Thavisha04 <thavisha.thanippuliappuhamilag@dcmail.ca>
2024-08-02 09:57:00 -0400

    a4p3-main.js version 6

--- assignments/assignment-4/part-3/a4p3-main.js
+++ assignments/assignment-4/part-3/a4p3-main.js
@@ -54,7 +54,22 @@ class Ball {
   
     this.x += this.velX;
     this.y += this.velY;
-  }  
+  }
+  
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  
 }
 
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
@@ -81,4 +96,21 @@ while (balls.length < 25) {
 
   balls.push(ball);
 }
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
+
+
 
